15cefce479564a6c71ed81bb5f5a7af4c5a3ce82
n4to4 <n4to4k@gmail.com>
2021-07-31 05:37:24 +0900

    json

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
-use axum::prelude::*;
+use axum::{extract::Json, prelude::*};
+use serde::Deserialize;
 use std::net::SocketAddr;
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+#[derive(Debug, Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) {
+    dbg!(&payload);
+}
 
 #[tokio::main]
 async fn main() {
-    let app = route("/", get(root));
+    let app = route("/", get(root)).route("/users", post(create_user));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     hyper::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
-}
-
-async fn root() -> &'static str {
-    "Hello, World!"
 }
