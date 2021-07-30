0377e631796fcdb4972ca9cc070bd07f4b566fd7
n4to4 <n4to4k@gmail.com>
2021-07-31 06:13:01 +0900

    tower

--- src/main.rs
+++ src/main.rs
@@ -37,19 +37,37 @@ async fn json() -> response::Json<Value> {
     response::Json(json!({"data":42}))
 }
 
+use axum::service;
+use http::Response;
+use tower::service_fn;
+use tower_http::services::ServeFile;
+
 #[tokio::main]
 async fn main() {
-    let app = route("/", get(root))
-        .route("/users", post(create_user))
-        .route("/text", get(text))
-        .route("/string", get(string))
-        .route("/404", get(not_found))
-        .route("/html", get(html))
-        .route("/json", get(json))
-        .route("/hc", get(|| async { "Ok" }));
+    //let app = route("/", get(root))
+    //    .route("/users", post(create_user))
+    //    .route("/text", get(text))
+    //    .route("/string", get(string))
+    //    .route("/404", get(not_found))
+    //    .route("/html", get(html))
+    //    .route("/json", get(json))
+    //    .route("/hc", get(|| async { "Ok" }));
+
+    let app = route(
+        "/",
+        service::any(service_fn(|_: Request<Body>| async {
+            let res = Response::new(Body::from("Hi from GET /"));
+            Ok::<_, std::io::Error>(res)
+        })),
+    )
+    .route(
+        "/static/Cargo.toml",
+        service::get(ServeFile::new("Cargo.toml")),
+    );
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     hyper::Server::bind(&addr)
+        //.serve(app.into_make_service())
         .serve(app.into_make_service())
         .await
         .unwrap();
